e79db950cfac1613fe714dbd956b4af94a093715
aliveGUY <ilyadubrivvin@gmail.com>
2022-06-16 19:17:19 +0300

    finish

--- src/index.test.js
+++ src/index.test.js
@@ -1,52 +1,47 @@
-import axios from 'axios';
-import React from 'react';
-import { rest } from 'msw';
-import { setupServer } from 'msw/node';
-import { configureStore } from '@reduxjs/toolkit';
+import renderer from 'react-test-renderer';
+import configureStore from 'redux-mock-store';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import { render as rtlRender, screen } from '@testing-library/react';
-import reducer, { joinMission } from './redux/missions/Redux-Missions';
-import App from './App';
+import '@testing-library/jest-dom';
+import Missions from './components/Missions';
 
-function render(
-  ui,
-  {
-    preloadedState,
-    store = configureStore({ reducer: { user: reducer }, preloadedState }),
-    ...renderOptions
-  } = {}
-) {
-  function Wrapper({ element }) {
-    return <Provider store={store}>{element}</Provider>;
-  }
-  return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
-}
-const handlers = [
-  rest.get('/api/user', (req, res, ctx) =>
-    res(ctx.json('John Smith'), ctx.delay(150))
-  ),
-];
-const server = setupServer(...handlers);
-// Enable API mocking before tests.
-beforeAll(() => server.listen());
+const mockStore = configureStore([]);
 
-// Reset any runtime request handlers we may add during the tests.
-afterEach(() => server.resetHandlers());
-
-// Disable API mocking after the tests are done.
-afterAll(() => server.close());
-
-test('Should return the initial state', () => {
-  expect(reducer(undefined, {})).toEqual([
+describe('Missions testing', () => {
+  const mission = [
     {
-      description: 'N/A',
       mission_id: 0,
-      mission_name: 'No Missions Yet',
+      mission_name: 'Mission Name',
+      description: 'Mission Description',
     },
-  ]);
-});
-
-test('should handle a mission being reserved', async () => {
-  render(<App />);
-  expect(screen.getByText(/Mission/i)).toBeInTheDocument();
+  ];
+  test('REnder Missions', () => {
+    const store = mockStore({
+      missions: mission,
+    });
+    const tree = renderer
+      .create(
+        <Provider store={store}>
+          <Missions />
+        </Provider>,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+  test('Should dispatch MissionReserve action', () => {
+    const store = mockStore({
+      missions: mission,
+    });
+    render(
+      <Provider store={store}>
+        <Missions />
+      </Provider>,
+    );
+    fireEvent.click(screen.getByText('Join Mission'));
+    const firedAction = store.getActions();
+    expect(...firedAction).toEqual({
+      payload: 0,
+      type: 'Missions/joinMission',
+    });
+  });
 });
