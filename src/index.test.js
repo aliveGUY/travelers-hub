fa4e5ab7267af533dbd9cfd0029410573b0b630f
aliveGUY <ilyadubrivvin@gmail.com>
2022-06-16 04:18:08 +0300

    Setup testing

--- src/index.test.js
+++ src/index.test.js
@@ -1,26 +1,52 @@
 import axios from 'axios';
-import '@testing-library/jest-dom';
+import React from 'react';
+import { rest } from 'msw';
+import { setupServer } from 'msw/node';
+import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from 'react-redux';
-import { render } from '@testing-library/react';
-import { loadMissions } from './redux/missions/Redux-Missions';
-import Missions from './components/Missions';
-import store from './redux/config';
+import { render as rtlRender, screen } from '@testing-library/react';
+import reducer, { joinMission } from './redux/missions/Redux-Missions';
+import App from './App';
 
-describe('Fetching Tests', () => {
-  it('Fetching Missions', async () => {
-    axios.post = jest.fn(() => Promise.resolve());
-    const dispatch = jest.fn();
+function render(
+  ui,
+  {
+    preloadedState,
+    store = configureStore({ reducer: { user: reducer }, preloadedState }),
+    ...renderOptions
+  } = {}
+) {
+  function Wrapper({ element }) {
+    return <Provider store={store}>{element}</Provider>;
+  }
+  return rtlRender(ui, { wrapper: Wrapper, ...renderOptions });
+}
+const handlers = [
+  rest.get('/api/user', (req, res, ctx) =>
+    res(ctx.json('John Smith'), ctx.delay(150))
+  ),
+];
+const server = setupServer(...handlers);
+// Enable API mocking before tests.
+beforeAll(() => server.listen());
 
-    await loadMissions()(dispatch);
-    expect(dispatch.mock.calls[1][0].type).toEqual('FETCH_MISSIONS/fulfilled');
-  });
+// Reset any runtime request handlers we may add during the tests.
+afterEach(() => server.resetHandlers());
 
-  it('Render Missions', async () => {
-    const { findByText } = render(
-      <Provider store={store}>
-        <Missions />
-      </Provider>,
-    );
-    expect(await findByText('Mission')).toBeInTheDocument();
-  });
+// Disable API mocking after the tests are done.
+afterAll(() => server.close());
+
+test('Should return the initial state', () => {
+  expect(reducer(undefined, {})).toEqual([
+    {
+      description: 'N/A',
+      mission_id: 0,
+      mission_name: 'No Missions Yet',
+    },
+  ]);
+});
+
+test('should handle a mission being reserved', async () => {
+  render(<App />);
+  expect(screen.getByText(/Mission/i)).toBeInTheDocument();
 });
